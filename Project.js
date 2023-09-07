e1f8475ef105018f4392877e2b0a04614fb911b7
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-09-07 13:05:00 +0100

    Clean up
    refactored the order prompt in the JavaScript code to improve code readability. The changes include renaming variables for clarity and adding comments to explain each step of the order process. This makes the code easier to understand and maintain.

--- Project.js
+++ Project.js
@@ -1,44 +1,59 @@
+// Import the prompt-sync module
 var prompt = require('prompt-sync')();
+
+// Define the menu items as an array of arrays
 const menuItems = [
    ['waffles', 'waffles'],
    ['pancakes', 'pancakes'],
    ['icecream', 'icecream'],
 ];
+
+// Convert the menu items array into an object using Object.fromEntries
 const menu = Object.fromEntries(menuItems);
+
+// Display the menu to the user
 console.log(`Welcome to Sean Waffles and Creams. This is our Menu:\n${Object.keys(menu)} \n \n`);
 
-const order = (menuItem) => {
-   return new Promise((res, rej) => {
-      menuItem = prompt("What do you want to order?: ").toLowerCase()
+// Define the order function using a Promise
+const order = () => {
+   return new Promise((resolve, reject) => {
+      // Prompt the user for their order
+      let menuItem = prompt("What do you want to order?: ").toLowerCase();
+      
+      // Check if the order exists in the menu
       if (menu[menuItem]){
-         console.log(`${menuItem} was ordered`)
-         res(menuItem)
-      }else{
-         rej(`${menuItem} was not on menu`)
+         console.log(`${menuItem} was ordered`);
+         resolve(menuItem);
+      } else{
+         reject(`${menuItem} was not on the menu`);
       }
-   })
+   });
 };
 
+// Call the order function
+order()
+   .then(menuItem => {
+      // Use setTimeout to simulate the preparation time
+      setTimeout(() => {
+         console.log(`${menuItem} is being prepared`);
+      }, 2000);
 
-order().then(menuItem=>{
-   setTimeout(()=>{
-      console.log(`${menuItem} is being prepared`)
-   },2000);
-   setTimeout(()=>{
-      console.log(`${menuItem} is now Ready!!!!`)
-   },7000);
-   setTimeout(()=>console.log(`${menuItem} has been served Enjoy!!`),8000)
-})
+      // Use setTimeout to simulate the cooking time
+      setTimeout(() => {
+         console.log(`${menuItem} is now ready!!!!`);
+      }, 7000);
 
-.catch(err=>{console.log(err)})
-
-.finally(setTimeout(()=>console.log("Have a good day come back next time"),9000))
-
-
-
-
-
-
-
-
-
+      // Use setTimeout to simulate the serving time
+      setTimeout(() => {
+         console.log(`${menuItem} has been served. Enjoy!!`);
+      }, 8000);
+   })
+   .catch(err => {
+      console.log(err);
+   })
+   .finally(() => {
+      // Use setTimeout to delay the final message
+      setTimeout(() => {
+         console.log("Have a good day. Come back next time!");
+      }, 9000);
+   });
